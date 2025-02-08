4f59c932f1858469aa0ab13d40ca951d81d0d4c1
kyogrekube <67924548+kyogrekube@users.noreply.github.com>
2025-02-07 22:43:37 -0500

    Pokeball rollout redirects to teambuilder

--- src/homepage.js
+++ src/homepage.js
@@ -1,7 +1,15 @@
+
 document.addEventListener("DOMContentLoaded", function () {
     const pokeball = document.querySelector(".pokeball");
 
     pokeball.addEventListener("click", function () {
+        // Apply the rollout animation to pokeball
         pokeball.style.animation = "rollOut 1.5s ease-in forwards";
+        // Once animation is finished, redirect to ./teambuilder
+        pokeball.addEventListener("animationend", function (event) {
+            if (event.animationName === "rollOut") {
+                window.location.href = "./teambuilder.html";
+            }
+        }, { once: true }); // Prevents the event from occuring more than once
     });
 });
