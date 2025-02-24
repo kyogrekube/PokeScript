2217dbe2673e45d645acbc5451151b49d3935a4f
kyogrekube <67924548+kyogrekube@users.noreply.github.com>
2025-02-24 00:39:36 -0500

    Abilities and Moveset are dynamically updated based on current selections

--- src/resources/editteam.js
+++ src/resources/editteam.js
@@ -1,9 +1,31 @@
+// Updates the newStatsContainer with the currently selected stats
+function updateNewStatsContainer(newStatsContainer) {
+    // Creates arrays from the nodelist of checked items in the possible moves and abilities containers
+    const selectedMoves = Array.from(document.querySelectorAll("#possibleMovesContainer .moveCheckbox:checked"))
+        .map(move => move.nextElementSibling.textContent);
+    const selectedAbility = Array.from(document.querySelectorAll("#possibleAbilitiesContainer .abilityCheckbox:checked"))
+        .map(ability => ability.nextElementSibling.textContent);
+    newStatsContainer.innerHTML = `<p><strong>Ability:</strong></p>`;
+    // Joins the abilities together and attches them to html
+    if (selectedAbility.length > 0) {newStatsContainer.innerHTML = newStatsContainer.innerHTML + `<p>${selectedAbility.join(", ")}</p>`}
+    else {newStatsContainer.innerHTML = newStatsContainer.innerHTML + `<p>None selected</p>`}
+    newStatsContainer.innerHTML = newStatsContainer.innerHTML + `<p><strong>Moveset:</strong></p>`;
+    // Joins the moves together and attches them to html
+    if (selectedMoves.length > 0) { newStatsContainer.innerHTML = newStatsContainer.innerHTML + `<p>${selectedMoves.join(", ")}</p>`}
+    else { newStatsContainer.innerHTML = newStatsContainer.innerHTML + `<p>None selected</p>` }
+    newStatsContainer.innerHTML = newStatsContainer.innerHTML + `<button class="saveStatsButton">Save Stat Changes</button>`
+}
+
 // Creates the display for the Moveset and Ability selection process
 function displayMovesetAndAbilitySelection(changeStatsContainer, newStatsContainer) {
     document.getElementById("editStatsButtonID").addEventListener("click", () => {
         const selectedPokemon = document.querySelectorAll("#possiblePokemonContainer .pokemonCheckbox:checked");
         // Ensures only 1 pokemon is selected at a time
-        if (selectedPokemon.length == 1) {
+        if (selectedPokemon.length != 1) {
+            alert("Exactly 1 pokemon may be selected at a time");
+            return;
+        }
+        else {
             // Creates moveset container
             const changeMovesetContainer = document.createElement("div");
             changeMovesetContainer.classList.add("changeMovesetContainer");
@@ -22,39 +44,36 @@ function displayMovesetAndAbilitySelection(changeStatsContainer, newStatsContain
             changeStatsContainer.innerHTML = "";
             changeStatsContainer.appendChild(changeMovesetContainer);
             changeStatsContainer.appendChild(changeAbilityContainer);
+            // Updates the stats container with current moves/abilities
+            updateNewStatsContainer(newStatsContainer);
 
-            // Displays newly selected stats
-            newStatsContainer.innerHTML = `
-                <p><strong>Ability:</strong></p>
-                <p>Truant</p>
-                <p><strong>Moveset:</strong></p> 
-                <p>Ember, Draco Meteor, Wood Hammer, Heat Crash</p>
-                <button class="saveStatsButton">Save Stat Changes</button>
-            `;
             // Prevents users from selecting more than 4 moves at once
-            document.getElementById("possibleMovesContainer").addEventListener("change", () => {
+            document.getElementById("possibleMovesContainer").addEventListener("change", (event) => {
                 const selectedMoves = document.querySelectorAll("#possibleMovesContainer .moveCheckbox:checked");
                 if (selectedMoves.length > 4) {
                     alert("A pokemon may only have up to 4 moves");
-                    selectedMoves[selectedMoves.length - 1].checked = false;
+                    event.target.checked = false;
                 }
+                // Updates the stats container with current moves/abilities
+                updateNewStatsContainer(newStatsContainer);
             });
             // Prevents users from selecting more than 1 ability at once
-            document.getElementById("possibleAbilitiesContainer").addEventListener("change", () => {
+            document.getElementById("possibleAbilitiesContainer").addEventListener("change", (event) => {
                 const selectedAbility = document.querySelectorAll("#possibleAbilitiesContainer .abilityCheckbox:checked");
                 if (selectedAbility.length > 1) {
                     alert("A pokemon may only have 1 ability");
-                    selectedAbility[selectedAbility.length - 1].checked = false;
+                    event.target.checked = false;
                 }
+                // Updates the stats container with current moves/abilities
+                updateNewStatsContainer(newStatsContainer);
             });
 
             // Populating lists of possible moves and abilities
             selectedPokemon.forEach(pokemon => {
-                fetch("https://pokeapi.co/api/v2/pokemon/"+pokemon.nextElementSibling.textContent)
+                fetch("https://pokeapi.co/api/v2/pokemon/" + pokemon.nextElementSibling.textContent)
                 .then(response => response.json())
                 .then(data => {
                     // Populating the left scrollable element with possible moves
-                    console.log(data);
                     data.moves.forEach(move => {
                         const checkBoxOption = document.createElement("div");
                         checkBoxOption.classList.add("checkBoxOption");
@@ -78,11 +97,6 @@ function displayMovesetAndAbilitySelection(changeStatsContainer, newStatsContain
                 });
             });
         }
-        // Since a user cannot ever have more than 1 pokemon selected, we can assume that the only other option is having none selected
-        else {
-            alert("A pokemon must be selected");
-            return;
-        }
     });
 }
 
@@ -102,15 +116,15 @@ function displayPokemonSelection() {
                 </div>
             `;
             // Prevents the user from selecting more than 1 pokemon at once
-            document.getElementById("possiblePokemonContainer").addEventListener("change", () => {
+            const possiblePokemonContainer = document.getElementById("possiblePokemonContainer");
+            possiblePokemonContainer.addEventListener("change", (event) => {
                 const selectedPokemon = document.querySelectorAll("#possiblePokemonContainer .pokemonCheckbox:checked");
                 if (selectedPokemon.length > 1) {
                     alert("Only 1 pokemon may be selected at a time");
-                    selectedPokemon[selectedPokemon.length - 1].checked = false;
+                    event.target.checked = false;
                 }
             });
             // Populates the list of possible pokemon to be chosen from
-            const possiblePokemonContainer = document.getElementById("possiblePokemonContainer");
             fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
                 .then(response => response.json())
                 .then(data => {
