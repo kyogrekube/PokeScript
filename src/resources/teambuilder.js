af350a99fb2e2f0bc1395b06b21110b0abe3d978
kyogrekube <67924548+kyogrekube@users.noreply.github.com>
2025-03-13 05:15:06 -0400

    Team Builder page now displays current state of teams

--- src/resources/teambuilder.js
+++ src/resources/teambuilder.js
@@ -6,3 +6,32 @@ function editTeam(teamKey) {
     localStorage.setItem("currentTeamKey", teamKey);
     window.location.href = "./editteam.html";
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    let teams = ["player", "enemy"];
+    for (let i = 0; i < 2; i++) {
+        const savedTeamInfo = JSON.parse(localStorage.getItem(teams[i] + "Team"))
+        console.log(savedTeamInfo);
+        for (let j = 0; j < 6; j++) {
+            const currentTeamMemberCard = document.getElementById(teams[i] + "TeamMemberCard" + (j + 1));
+            currentTeamMemberCard.innerHTML = `
+                <img src="${savedTeamInfo[j].imageURL}" alt="${savedTeamInfo[j].name + "Sprite"}">
+                <p><strong>${savedTeamInfo[j].name}</strong></p>
+            `
+            if (savedTeamInfo[j].ability == "") {
+                currentTeamMemberCard.innerHTML = currentTeamMemberCard.innerHTML + `<p><strong>Ability:</strong> N/A</p>`;
+            }
+            else {
+                currentTeamMemberCard.innerHTML = currentTeamMemberCard.innerHTML + `<p><strong>Ability:</strong> ${savedTeamInfo[j].ability}</p>`;
+            }
+            for (let k = 1; k <= 4; k++) {
+                if (k > savedTeamInfo[j].moves.length) {
+                    currentTeamMemberCard.innerHTML = currentTeamMemberCard.innerHTML + `<p><strong>Move ${k}:</strong> N/A</p>`;
+                }
+                else {
+                    currentTeamMemberCard.innerHTML = currentTeamMemberCard.innerHTML + `<p><strong>Move ${k}:</strong> ${savedTeamInfo[j].moves[k-1]}</p>`;
+                }
+            }
+        }
+    }
+});
