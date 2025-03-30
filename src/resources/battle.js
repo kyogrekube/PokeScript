bf62a2559b961d274a60c8416082e25d2a1e78f1
kyogrekube <67924548+kyogrekube@users.noreply.github.com>
2025-03-29 22:25:04 -0400

    creating action menu

---
+++ src/resources/battle.js
@@ -0,0 +1,39 @@
+// Loads the action menu and any changes on it
+function loadActionMenu() {
+    const container = document.getElementById("ActionMenu");
+    container.innerHTML = `
+        <div class = "topInnerActionMenu">
+            <button class = "actionMenuButton fight" id = "fightButton">FIGHT</button>
+        </div>
+        <div class = "bottomInnerActionMenu">
+            <div class = "bottomLeftInnerActionMenu">
+            <button class = "actionMenuButton bag" id = "bagButton">BAG</button>
+            </div>
+            <div class = "bottomRightInnerActionMenu">
+            <button class = "actionMenuButton pokemon" id = "pokemonButton">POKEMON</button>
+            </div>
+        </div>
+    `;
+
+    const fightBtn = document.getElementById("fightButton");
+    const bagBtn = document.getElementById("bagButton");
+    const pokemonBtn = document.getElementById("pokemonButton");
+
+    fightBtn.addEventListener("click", () => {
+        alert("Currently under development!");
+    });
+
+    bagBtn.addEventListener("click", () => {
+        alert("Currently under development!");
+    });
+
+    pokemonBtn.addEventListener("click", () => {
+        alert("Currently under development!");
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    loadActionMenu();
+    console.log(localStorage);
+});
+  
